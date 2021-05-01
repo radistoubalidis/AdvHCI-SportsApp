ace0891a6d663fd8b18da54d4f18ed2b02132856
Andrew Rapessis <andrewrapessis@gmail.com>
2021-05-01 18:09:18 +0300

    Firestore and Function Check
    Firestore Connected // Match Saving function testing

--- app/src/main/java/com/example/drawerfragment/MainActivity.java
+++ app/src/main/java/com/example/drawerfragment/MainActivity.java
@@ -9,11 +9,29 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Constants for Firestore
+    public static final String SPORT_KEY = "sport";
+    public static final String COUNTRY_KEY = "country";
+    public static final String CITY_KEY = "city";
+
+    //Firestore Init
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("androidproj-9ef3f/Agones");
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -33,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.navigationView);
         navigationView.setNavigationItemSelectedListener(this);
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
@@ -43,26 +61,62 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // load default fragment
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.container_fragment,new MainFragment());
+        fragmentTransaction.add(R.id.container_fragment, new MainFragment());
         fragmentTransaction.commit();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.sport1){
+        if (item.getItemId() == R.id.sport1) {
             fragmentManager = getSupportFragmentManager();
             fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.container_fragment,new MainFragment());
+            fragmentTransaction.replace(R.id.container_fragment, new MainFragment());
             fragmentTransaction.commit();
         }
-        if (item.getItemId() == R.id.sport2){
+        if (item.getItemId() == R.id.sport2) {
             fragmentManager = getSupportFragmentManager();
             fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.container_fragment,new FragmentSecond());
+            fragmentTransaction.replace(R.id.container_fragment, new FragmentSecond());
             fragmentTransaction.commit();
         }
 
 
         return true;
     }
+
+    //Save a match function, needs UI and text fields to function
+  public void saveMatch(View view) {
+
+        EditText cityView = (EditText) findViewById(R.id.editTextCity);
+        EditText countryView = (EditText) findViewById(R.id.editTextCountry);
+        EditText sportView = (EditText) findViewById(R.id.editTextSport);
+        String cityText = cityView.toString();
+        String countryText = countryView.toString();
+        String sportText = sportView.toString();
+
+
+
+        if (cityText.isEmpty() || countryText.isEmpty() || sportText.isEmpty()) { return; }
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+
+        //Key value pairs with constants
+        dataToSave.put(CITY_KEY, cityText);
+        dataToSave.put(COUNTRY_KEY, countryText);
+        dataToSave.put(SPORT_KEY, sportText);
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("City","Sport");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("City","Document wasn't saved");
+            }
+        });
+
+
+    }
+
+
 }
