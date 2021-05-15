9865573e5d556964bc6f3fd4c23f17f8275763ce
radis toubalidis <rtoumpalidis@gmail.com>
2021-05-15 20:55:09 +0300

    mm

--- app/src/main/java/com/example/drawerfragment/AddMatchupFragment.java
+++ app/src/main/java/com/example/drawerfragment/AddMatchupFragment.java
@@ -2,12 +2,14 @@ package com.example.drawerfragment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,25 +20,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddMatchupFragment extends Fragment {
     View view;
+    FragmentManager fragmentManager;
+    FragmentTransaction transaction;
     Spinner spinner,athletesSpinner;
-    EditText et1,et2;
+    EditText team1,team2,date,country,city;
     TextView athleteInserted,athletes;
     ArrayList<String>  selectedAthletes = new ArrayList<String>();
+    int athletesCounter=0;
+    double [] apodoseis = new double [8];
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_insert_matchup, container, false);
         spinner =(Spinner) view.findViewById(R.id.spinner);
+        Button add = (Button) view.findViewById(R.id.addMatcup);
+        // field.getText().toString()
+        EditText date  = (EditText) view.findViewById(R.id.date);
+        EditText country = (EditText) view.findViewById(R.id.country);
+        EditText city = (EditText) view.findViewById(R.id.city);
+
+        // dropdown list twn athlitwn
         athletesSpinner = (Spinner) view.findViewById(R.id.athleteSpinner);
-        et1 = (EditText) view.findViewById(R.id.et1);
-        et2 = (EditText) view.findViewById(R.id.et2);
+        team1 = (EditText) view.findViewById(R.id.et1);
+        team2 = (EditText) view.findViewById(R.id.et2);
         athleteInserted = (TextView) view.findViewById(R.id.athletesInserted);
         athletes = (TextView) view.findViewById(R.id.tv1);
         String [] sports = MainActivity.db.sportsDAO().returnSportsNames();
@@ -57,7 +74,7 @@ public class AddMatchupFragment extends Fragment {
                         break;
                     }
                 }
-                changeVisibility(athletesSpinner,athletes,athleteInserted,et1,et2,type);
+                changeVisibility(athletesSpinner,athletes,athleteInserted,team1,team2,type);
                 if(type.equals("Individual")){
                     ArrayList<String> names = setupIndividual(sid);
 
@@ -72,7 +89,12 @@ public class AddMatchupFragment extends Fragment {
             }
         });
 
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
         return view;
     }
@@ -121,7 +143,14 @@ public class AddMatchupFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String athlete = athletesSpinner.getSelectedItem().toString();
                 String textView = athleteInserted.getText().toString();
-                textView += athlete + " , " ;
+                double score = (double)(Math.random()*(2+5+1)+2);
+                DecimalFormat df = new DecimalFormat("##.##");
+                apodoseis[athletesCounter] = Double.parseDouble(df.format(score));
+                athletesCounter++;
+
+                selectedAthletes.add(athlete);
+                Log.i("team",selectedAthletes.toString());
+                textView += athlete+ " , ";
                 athleteInserted.setText(textView);
             }
 
